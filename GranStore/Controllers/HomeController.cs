cab6ef553ba00922af400510613907eb33103968
andradebeatriz351@gmail.com <andradebeatriz351@gmail.com>
2025-08-25 20:41:28 -0300

    Criada a página de detalhes

--- GranStore/Controllers/HomeController.cs
+++ GranStore/Controllers/HomeController.cs
@@ -7,21 +7,19 @@ namespace GranStore.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private List<Produto> produtos; 
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
 
-    public IActionResult Index()
-    {
         List<Categoria> categorias = new()
         {
             new Categoria { Id = 1, Nome = "Livro"},
             new Categoria { Id = 2, Nome = "Marcadores"},
         };
 
-        List<Produto> produtos = new()
+        produtos = new List<Produto>
         {
             new Produto { Id = 1, Nome = "Café com teu Pai", Descricao = "Livro de Ajuda", Categoria = categorias[0], QtdeEstoque = 13, ValorCusto = 100.00m, ValorVenda = 200.00m, Foto = "/img/produtos/1.png"},
             new Produto { Id = 2, Nome = "Marcador de Coração", Descricao = "Marcador util para livros", Categoria = categorias[1], QtdeEstoque = 13, ValorCusto = 100.00m, ValorVenda = 200.00m, Foto = "/img/produtos/2.png"},
@@ -32,8 +30,17 @@ public class HomeController : Controller
             new Produto { Id = 7, Nome = "Marcador com Textura Xadrez", Descricao = "Marcador util para livros", Categoria = categorias[1], QtdeEstoque = 13, ValorCusto = 100.00m, ValorVenda = 200.00m, Foto = "/img/produtos/7.png"},
             new Produto { Id = 8, Nome = "Marcador de Xicara de Café", Descricao = "Marcador util para livros", Categoria = categorias[1], QtdeEstoque = 13, ValorCusto = 100.00m, ValorVenda = 200.00m, Foto = "/img/produtos/8.png"},
         };
+    }
 
-        return View();
+    public IActionResult Index()
+    {
+        return View(produtos);
+    }
+
+    public IActionResult Produto(int id)
+    {
+        var produto = produtos.SingleOrDefault(p => p.Id == id);
+        return View(produto);
     }
 
     public IActionResult Privacy()
